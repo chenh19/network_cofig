2ac4a2f5b6090ae0461a92add1e88c386603a8eb
Hang Chen <chen_h@outlook.com>
2023-03-20 18:16:58 -0500

    Add files via upload

---
+++ config.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# This script helps configure network settings
+
+bash <(wget -qO- https://raw.githubusercontent.com/chenh19/network_cofig/main/src/home-wifi.sh)
+bash <(wget -qO- https://raw.githubusercontent.com/chenh19/network_cofig/main/src/university-wifi.sh)
+bash <(wget -qO- https://raw.githubusercontent.com/chenh19/network_cofig/main/src/university-vpn.sh)
