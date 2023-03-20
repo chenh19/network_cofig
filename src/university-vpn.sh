39c571591c5487b8433edf1b9f8f156b652b92a1
Hang Chen <chen_h@outlook.com>
2023-03-20 18:24:32 -0500

    Update university-vpn.sh

--- src/university-vpn.sh
+++ src/university-vpn.sh
@@ -24,6 +24,7 @@ case "$choice" in
 
         # notify end
         echo -e " \n${TEXT_GREEN}University VPN configuration finished!${TEXT_RESET} \n" && sleep 1;;
+        
   * )   # notify cancellation
         echo -e " \n${TEXT_YELLOW}University VPN configuration skipped.${TEXT_RESET} \n" && sleep 1;;
 esac
