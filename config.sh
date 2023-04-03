8136ac54ba14f0197ad68137a9a6d044668e6244
Hang Chen <chen_h@outlook.com>
2023-04-03 00:48:55 -0500

    Update config.sh

--- config.sh
+++ config.sh
@@ -2,5 +2,6 @@
 # This script helps configure network settings
 
 bash <(wget -qO- https://raw.githubusercontent.com/chenh19/network_cofig/main/src/home-wifi.sh)
+bash <(wget -qO- https://raw.githubusercontent.com/chenh19/network_cofig/main/src/mobile-wwan.sh)
 bash <(wget -qO- https://raw.githubusercontent.com/chenh19/network_cofig/main/src/university-wifi.sh)
 bash <(wget -qO- https://raw.githubusercontent.com/chenh19/network_cofig/main/src/university-vpn.sh)
