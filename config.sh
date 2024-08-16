38e9d752ef18d1bab5743c931afdc8e235a4dae3
Hang Chen <chen_h@outlook.com>
2024-08-16 11:51:00 -0500

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # This script helps configure network settings
 
+if ! dpkg -l | grep -q "^ii.*wget" ; then sudo apt-get update -qq && sudo apt-get install wget -y && sleep 1 ; fi
 bash <(wget -qO- https://raw.githubusercontent.com/chenh19/network_cofig/main/src/home-wifi.sh)
 bash <(wget -qO- https://raw.githubusercontent.com/chenh19/network_cofig/main/src/mobile-wwan.sh)
 bash <(wget -qO- https://raw.githubusercontent.com/chenh19/network_cofig/main/src/mobile-hotspot.sh)
